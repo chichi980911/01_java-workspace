c1b61960f954584672f011d03a87fb83b2e12d69
chichi980911 <shkim3657@nate.com>
2023-05-29 17:50:28 +0900

    1

--- 02_Operator/src/com/kh/run/operatorRun.java
+++ 02_Operator/src/com/kh/run/operatorRun.java
@@ -1,25 +1,53 @@
-package com.kh.run;
-
-//import com.kh.operator.A_LogicalNegation;
-
-//import com.kh.operator.B_InDecrease;
-import com.kh.operator.C_Arithmetic;
-
-public class operatorRun {
-	public static void main(String[] args) {
-//		A_LogicalNegation a = new A_LogicalNegation();
-//		a.method1();
-		
-//		B_InDecrease b1 = new B_InDecrease();
-//		b1.method1();
-////		b1.method2();
-////		b1.method3();
-//		b1.method4();
+////package com.kh.run;
+//
+////import com.kh.operator.G_Triple;
+//
+////import com.kh.operator.D_comparison;
+////import com.kh.operator.E_Logical;
+////import com.kh.operator.F_Compound;
+//
+////import com.kh.operator.A_LogicalNegation;
+//
+////import com.kh.operator.B_InDecrease;
+////import com.kh.operator.C_Arithmetic;
+//
+//public class operatorRun {
+//	public static void main(String[] args) {
+////		A_LogicalNegation a = new A_LogicalNegation();
+////		a.method1();
 //		
-		C_Arithmetic c1 = new C_Arithmetic();
-		c1.method1();
-		
-			
-	}
-
-}
+////		B_InDecrease b1 = new B_InDecrease();
+////		b1.method1();
+//////		b1.method2();
+//////		b1.method3();
+////		b1.method4();
+////		
+////		C_Arithmetic c1 = new C_Arithmetic();
+////		c1.method1();
+//		
+////		D_comparison d = new D_comparison();
+////		d.method2();
+//		
+////		E_Logical e = new E_Logical();
+//////		e.method1();
+//////		e.method2();
+//////		e.method3();
+//////		e.method4();
+////		e.method5();
+//		
+////		F_Compound f = new F_Compound();
+////		f.method1();
+//		
+////		G_Triple g = new G_Triple();
+//////		g.method2();
+//////		g.method3();
+//////		g.method4();
+//////		g.method5();
+////		g.method6();
+//		
+//		
+//		
+//			
+//	}
+//
+//}
